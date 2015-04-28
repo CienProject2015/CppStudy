2ca579111aceb542f68104683fd5c69bf4a110f7
HoraceVelmont <dragond7@naver.com>
2015-04-28 14:52:24 +0900

    Create HelloWorld.cpp

---
+++ HelloWorld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(void)
+{
+  int val1;
+  std::cout << "Hello World" << std::endl;
+  std::cout << "Hello " << "World!"<<std::endl;
+  std::cout << num << ' ' << 'A';
+  std::cout << ' ' << 3.14 << std::endl;
+  return 0
+}
