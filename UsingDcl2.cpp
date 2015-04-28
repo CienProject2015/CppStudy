65081296568dfd0acfca7a9690ad97d838e419cb
HoraceVelmont <dragond7@naver.com>
2015-04-28 16:04:49 +0900

    Create UsingDcl2.cpp

---
+++ UsingDcl2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std; //using std::cout; using std::endl;
+
+int main(void)
+{
+  int num=208;
+  cout<<"Hello World!"<<endl;
+  cout<<num<<' '<<'A';
+  return 0;
+}
