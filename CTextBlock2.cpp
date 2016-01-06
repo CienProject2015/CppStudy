fda9ebf9a6d0edc5a97c85a1aacf48556f5190f7
HoraceVelmont <dragond7@naver.com>
2016-01-06 15:02:39 +0900

    Update CTextBlock2.cpp

--- CTextBlock2.cpp
+++ CTextBlock2.cpp
@@ -5,7 +5,7 @@ public:
   
 private:
   char *pText;
-  mutable std:size_t textLength;
+  mutable std:size_t textLength; //이 데이터 멤버들은 어떤 순간에도 수정이 가능하다. 상수 멤버 함수 안에서도 수정할 수 있다.
   mutable bool lengthIsValid;
 };
 
