0f3dcc3dd8cd14fc8a4849f59eb9ff0d3ce6d65d
HoraceVelmont <dragond7@naver.com>
2016-01-06 15:01:14 +0900

    Update CTextBlock.cpp

--- CTextBlock.cpp
+++ CTextBlock.cpp
@@ -2,13 +2,13 @@ class CTextBlock
 {
 public:
 
-	...
+  ...
 	
-	char& operator[](std::size_t position) const //부적절하지만 비트수준의 상수성 때문에 허용된다.
-	{
-		return pText[position];
-	}
+  char& operator[](std::size_t position) const //부적절하지만 비트수준의 상수성 때문에 허용된다.
+  {
+    return pText[position];
+  }
 
 private:
-	char *pTest;
+  char *pTest;
 };
