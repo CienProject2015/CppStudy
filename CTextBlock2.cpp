669aea749fef011ee3c1cd8ddd8c82fdc8365cb0
HoraceVelmont <dragond7@naver.com>
2016-01-06 14:58:57 +0900

    Create CTextBlock2.cpp

---
+++ CTextBlock2.cpp
@@ -0,0 +1,20 @@
+class CTextBlock{
+public:
+  ...
+  std::size_t length() const;
+  
+private:
+  char *pText;
+  mutable std:size_t textLength;
+  mutable bool lengthIsValid;
+};
+
+std::size_t CTextBlock::length() const
+{
+  if(!lengthIsValid){
+    textLength = std::strlen(pText);
+    lengthIsValid = true;
+  }
+  
+  return textLength;
+}
