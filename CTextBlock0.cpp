b724a31a5521b3ad71bc861bb28412cbffa7d003
HoraceVelmont <dragond7@naver.com>
2016-01-06 15:28:40 +0900

    Create CTextBlock0.cpp

---
+++ CTextBlock0.cpp
@@ -0,0 +1,21 @@
+class TextBlock{
+  public:
+    ...
+    const char& operator[](std::size_t position) const // 상수 객체에 대한 operator[]
+    { 
+      return text[position];
+    }
+    
+    char& operator[](std::size_t position)
+    {
+      return text[position];
+    }
+    
+  private:
+    std::string text;
+};
+
+void print(const TextBlock& ctb)
+{
+  std::cout << ctb[0];
+}
